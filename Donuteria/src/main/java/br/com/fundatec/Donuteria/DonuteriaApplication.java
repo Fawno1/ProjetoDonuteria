b5d682b215a94eadc0efa988b4f106558fb683c5
Fawno1 <maissaude.equipe@gmail.com>
2019-12-16 19:57:06 -0300

    Criação do projeto inicial

---
+++ Donuteria/src/main/java/br/com/fundatec/Donuteria/DonuteriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fundatec.Donuteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonuteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonuteriaApplication.class, args);
+	}
+
+}
